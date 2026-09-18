22a205f41e0860587268701db0e3d70655945c5b
Ronald Tse <ronald.tse@ribose.com>
2026-09-18 18:25:36 +0800

    Untrack dist-test build output (it polluted every branch switch and blocked rebases)

--- dist-test/src/index.js
+++
@@ -1,6 +0,0 @@
-export * from "./corpus/types.js";
-export { loadCorpus } from "./corpus/loader.js";
-export { buildArtifact, loadBundledCorpus, parseArtifact, } from "./corpus/artifact.js";
-export { PendingRegistry } from "./conformance/pending.js";
-export { checkCase, runFlavor, runCorpus } from "./conformance/runner.js";
-export { corpusModeImplementation, CorpusBackedIdentifier } from "./implementations/corpus-mode.js";
